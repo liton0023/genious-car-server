a7ca2b8751a70f37787ca15fcce71afdb3ea6962
Md Liton Mahmud <litonmahmud0023@gmail.com>
2023-06-22 00:09:42 +0600

    added daynamic api

--- index.js
+++ index.js
@@ -25,14 +25,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   const serviceCollection=client.db('geniouscar').collection('services');
+
+   app.get('/services', async(req,res)=>{
+    const quarry={};
+    const cursor=serviceCollection.find(quarry);
+    const services= await cursor.toArray();
+    res.send(services);
+   })
+
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  
   }
 }
 run().catch(console.dir);
